31c273f1ca9660f1f0c95e502bd4934420c7b00e
Souzadfs <souzadf09@gmail.com>
2025-09-28 12:17:55 -0300

    modelando os dados, colunas cancelado p/ booleano

--- analise_flight.py/analise.py
+++ analise_flight.py/analise.py
@@ -2,3 +2,28 @@ import pandas as pd
 
 df = pd.read_csv("voos_comerciais_ficticios.csv")
 print(df.shape)
+
+#mostrando as 5 primeiras linhas do dataset
+print('Primeiras linhas do dataset')
+print(df.head())
+
+#Verificando os tipo de dados de cada coluna
+print(df.dtypes)
+
+#verificando se existe valores nulos
+print(df.isnull().sum())
+
+#Transformando a coluna 'Cancelado' em booleano
+df['Cancelado'] = df['Cancelado'].map({'Sim':True, 'Não': False})
+
+def categorizar_atraso(minutos):
+    if minutos == 0:
+        return 'Sem atraso'
+    elif 1 <= minutos <= 30:
+        return 'Leve'
+    
+    elif 30 <= minutos <= 90:
+        return 'Moderado'
+    
+    else:
+        return 'Grave'
